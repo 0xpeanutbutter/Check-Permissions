f1b0dc5b430592f5e77b65c81187f735c509f018
kineolyan <kineolyan@protonmail.com>
2021-10-03 22:23:16 +0200

    Refactor main activity.

--- app/src/main/java/com/example/permissionsapp/MainActivity.java
+++ app/src/main/java/com/example/permissionsapp/MainActivity.java
@@ -8,15 +8,18 @@ import android.content.pm.PermissionInfo;
 import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.lang.reflect.Field;
 import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -24,69 +27,108 @@ import static android.content.pm.PackageManager.GET_META_DATA;
 
 public class MainActivity extends AppCompatActivity {
 
-    ArrayList <String> inputList = new ArrayList<>();
-    List <String> chosen_per_list;
-    Map<String, String> permApp = new HashMap<String, String>() {};
+    private static final String GET_APPS_KEY = "Get apps";
+
+    private Map<String, String> permissions;
+    private List<String> chosenPerList;
 
     @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN_MR2)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ListView listView = findViewById(R.id.listview);
+        final ListView listView = findViewById(R.id.listview);
 
-        chosen_per_list= new ArrayList<>();
-        Field[] fields = Manifest.permission.class.getFields();
+        // Reset the activity state
+        this.chosenPerList = new ArrayList<>();
+        this.permissions = findAppPermissions();
+
+        final ArrayAdapter<String> adapter = createAdapter(permissions);
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(this::selectAppOrLoadPermissions);
+    }
+
+    private Map<String, String> findAppPermissions() {
+        final Field[] fields = Manifest.permission.class.getFields();
+        // Retain the order for consistency
+        final Map<String, String> permissionMap = new LinkedHashMap<>();
         for (Field field : fields) {
             try {
-                String permName = (String) field.get(Manifest.permission.class);
-                PermissionInfo permissionInfo = this.getPackageManager().getPermissionInfo(permName, GET_META_DATA);
-                String [] trimmedPerm = permissionInfo.name.split("\\.",3);
-                if(trimmedPerm.length >=1){
-                    permApp.put(trimmedPerm[trimmedPerm.length-1],permissionInfo.name);
-                    inputList.add(trimmedPerm[trimmedPerm.length-1]);
-                    Log.d("Value", trimmedPerm[trimmedPerm.length-1]);
+                final String permName = (String) field.get(Manifest.permission.class);
+                final PermissionInfo permissionInfo = getPackageManager().getPermissionInfo(permName, GET_META_DATA);
+                final String[] trimmedPerm = permissionInfo.name.split("\\.", 3);
+                if (trimmedPerm.length >= 1) {
+                    final String permissionId = trimmedPerm[trimmedPerm.length - 1];
+                    permissionMap.put(permissionId, permissionInfo.name);
+                    Log.d("Value", permissionId);
                 }
-
-            }
-            catch (PackageManager.NameNotFoundException | IllegalAccessException e) {
+            } catch (PackageManager.NameNotFoundException | IllegalAccessException e) {
                 e.printStackTrace();
             }
-
         }
-        inputList.add(0,"Get apps");
-        final ArrayAdapter<String> adapter = new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,inputList);
-        listView.setAdapter(adapter);
-        listView.setOnItemClickListener((parent, view, k, l) -> {
+        return permissionMap;
+    }
 
-            String selected_perm= (String) parent.getItemAtPosition(k);
-            if(selected_perm.equals("Get apps")) {
-                String[] Permissions = chosen_per_list.toArray(new String[0]);
-                try{
-                    List<PackageInfo> listOfApplications = getPackageManager().getPackagesHoldingPermissions(Permissions,PackageManager.GET_META_DATA);
-                    String[] packages = new String[listOfApplications.size()];
-                    int j=0;
-                    for(PackageInfo apps : listOfApplications) {
-                        packages[j]=apps.packageName;
-                        j++;
-                    }
-                    Log.e("packages",packages.length+"");
-                    Intent intent = new Intent(MainActivity.this, DisplayApps.class);
-                    Bundle bundle = new Bundle();
-                    bundle.putStringArray("packages",packages);
-                    intent.putExtras(bundle);
-                    startActivity(intent);
+    private ArrayAdapter<String> createAdapter(
+            final Map<String, String> permissions) {
+        final List<String> inputList = new ArrayList<>();
+        inputList.add(GET_APPS_KEY);
+        inputList.addAll(permissions.keySet());
+        return new ArrayAdapter<>(
+                this,
+                android.R.layout.simple_list_item_1,
+                inputList);
+    }
 
-                }
-                catch (Exception e){
-                    //
-                }
-                return;
-            }
-            chosen_per_list.add(permApp.get(selected_perm));
-        });
-
+    private void selectAppOrLoadPermissions(
+            final AdapterView<?> parent,
+            final View view,
+            final int position,
+            final long l) {
+        final String selected_perm = (String) parent.getItemAtPosition(position);
+        if (selected_perm.equals(GET_APPS_KEY)) {
+            showAppPermissions();
+        } else {
+            recordPermissionChoice(selected_perm);
         }
+    }
+
+    private void showAppPermissions() {
+        try {
+            final String[] packages = collectChosenPackages(this.chosenPerList);
+            Log.e("packages", packages.length + "");
+            final Intent intent = createIntent(packages);
+            startActivity(intent);
+        } catch (final RuntimeException e) {
+            Log.e("packages", "Error fetching info", e);
+        }
+    }
+
+    @NonNull
+    private String[] collectChosenPackages(final List<String> chosenPermissions) {
+        final String[] Permissions = chosenPermissions.toArray(new String[0]);
+        final List<PackageInfo> listOfApplications =
+                getPackageManager().getPackagesHoldingPermissions(
+                        Permissions,
+                        PackageManager.GET_META_DATA);
+        return listOfApplications.stream()
+                .map(app -> app.packageName)
+                .toArray(String[]::new);
+    }
+
+    private Intent createIntent(String[] packages) {
+        final Bundle bundle = new Bundle();
+        bundle.putStringArray("packages", packages);
+
+        final Intent intent = new Intent(this, DisplayApps.class);
+        intent.putExtras(bundle);
+
+        return intent;
+    }
+
+    private void recordPermissionChoice(final String choice) {
+        this.chosenPerList.add(permissions.get(choice));
+    }
 
 }
 
